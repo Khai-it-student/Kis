ff619935148bae6743ef6cc7900e31b283a0cafe
Khai-it-student <93523187+Khai-it-student@users.noreply.github.com>
2021-12-12 15:46:50 +0200

    Трішки інтерфейсу і трішки БД

---
+++ Kis/Form1.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using SautinSoft.Document;
+using System.Windows.Forms;
+
+namespace Kis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
